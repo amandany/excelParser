849e227f367ae19158220074b2df9b3f05f50e62
Pavel <pas2515@yandex.ru>
2021-03-29 23:38:41 +0300

    mapping

--- excelParser/Item.cs
+++ excelParser/Item.cs
@@ -8,8 +8,8 @@ namespace excelParser
 {
     class Item
     {
-        public int Prop1 { get; set; }
-        public string Prop2 { get; set; }
-        public string Prop3 { get; set; }
+        public double Prop1;
+        public string Prop2;
+        public string Prop3;
     }
 }
--- excelParser/Program.cs
+++ excelParser/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace excelParser
 {
@@ -15,9 +16,18 @@ namespace excelParser
         static void Main(string[] args)
         {
             //Item a = new Item();
-            //var a = typeof(Item);
+            var a = typeof(Item);
+            var b = a.GetFields();
+
+            Dictionary<string, int?> map = new Dictionary<string, int?>();
+
+            foreach (var fieldInfo in b)
+            {
+                map.Add(fieldInfo.Name, null);
+            }
+
             Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"D:\1REPOS\excelParser\excelParser\ex.xlsx");
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\Neerd\source\repos\excel\excelParser\excelParser\ex.xlsx");
             Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
             Excel.Range xlRange = xlWorksheet.UsedRange;
 
@@ -28,43 +38,70 @@ namespace excelParser
             Console.WriteLine(xlRange.Columns.Count);
             Console.WriteLine("///////////");
 
+
+
             int rowCount = xlRange.Rows.Count;
             int colCount = xlRange.Columns.Count;
-            for (int i = 1; i <= rowCount; i++)
-            {
-                string[] myArr = new string[colCount];
-                string line = "";
-                string tempLine = "";
-                for (int j = 1; j <= colCount; j++)
-                {
-                    //new line
-                    if (j == 1)
-                        Console.Write("\n");
 
-                    //write the value to the console
-                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
-                    {
-                        // Each elem in console
-                        // Console.Write(xlRange.Cells[i, j].Value2.ToString() + " ");
-                        tempLine = xlRange.Cells[i, j].Value2.ToString();
-                        myArr[j-1] = tempLine;
-                        Console.WriteLine(" ADD TO myArr: {0}", tempLine);
-                    }
-                    line = line + tempLine + " ";
+
+            for (int i = 1; i <= colCount; i++)
+            {
+                string c = xlRange.Cells[1, i].Value2.ToString();
+                if (map.ContainsKey(c))
+                {
+                    map[c] = i;
+                }
+            }
+
+
+
+
+            List<Item> items = new List<Item>();
+
+            for (int i = 2; i <= rowCount; i++)
+            {
+                Item item = new Item();
+
+                foreach (var mappedElement in map)
+                {
+                    FieldInfo fieldInfo = typeof(Item).GetField(mappedElement.Key);
+
+                    fieldInfo.SetValue(item, xlRange.Cells[i, mappedElement.Value].Value2);
 
                 }
-                listLines.Add(myArr);
-                Console.WriteLine(line);
+
+                items.Add(item);
+
+                //string[] myArr = new string[colCount];
+                //string line = "";
+                //string tempLine = "";
+                //for (int j = 1; j <= colCount; j++)
+                //{
+                //    //new line
+                //    if (j == 1)
+                //        Console.Write("\n");
+
+                //    //write the value to the console
+                //    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                //    {
+                //        // Each elem in console
+                //        // Console.Write(xlRange.Cells[i, j].Value2.ToString() + " ");
+                //        tempLine = xlRange.Cells[i, j].Value2.ToString();
+                //        myArr[j-1] = tempLine;
+                //        Console.WriteLine(" ADD TO myArr: {0}", tempLine);
+                //    }
+                //    line = line + tempLine + " ";
+
+                //}
+                //listLines.Add(myArr);
+                //Console.WriteLine(line);
             }
 
             Console.WriteLine("--------------LIST LINES----------------");
-            foreach (var item in listLines)
+            foreach (var item in items)
             {
-                foreach (var i in item)
-                {
-                    Console.Write(i + " ");
-                }
-                Console.WriteLine();
+                
+                Console.WriteLine($"{item.Prop1} | {item.Prop2} | {item.Prop3}");
             }
 
 
